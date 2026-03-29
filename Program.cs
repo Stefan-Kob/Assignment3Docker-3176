53feb921243a52f265f92126b9ea51d6857cf00c
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2026-03-29 14:50:35 -0400

    / verify endpoint added

--- Program.cs
+++ Program.cs
@@ -1,41 +1,22 @@
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/verify", () =>
 {
-    app.MapOpenApi();
-}
+    var runner = Environment.GetEnvironmentVariable("RUN_BY") ?? "NOT_FOUND";
+    var builderName = "Assignment 3 builder";
 
-app.UseHttpsRedirection();
+    var response = new
+    {
+        builder = builderName,
+        runner = runner,
+        timestamp = DateTime.UtcNow.ToString("o"),
+        machineName = Dns.GetHostName()
+    };
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return Results.Json(response);
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run("http://0.0.0.0:8080");
